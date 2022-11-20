16599442edf10f1b5f1d5c95e7282a42caf4297e
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2022-11-20 22:03:25 +0100

    Added @PathVariable to allow query through path (i.e. /kanban/1) instead of QueryParameter

--- api/src/main/java/es/capgemini/cca/canbemini/kanban/KanbanController.java
+++ api/src/main/java/es/capgemini/cca/canbemini/kanban/KanbanController.java
@@ -3,20 +3,24 @@ package es.capgemini.cca.canbemini.kanban;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KanbanController {
 
-    private static final String template = "Hello, %s!";
-
-
     @Autowired
     KanbanRepository repository;
 
+    @GetMapping("/api/kanban/{id}")
+    public Kanban getKanban(@PathVariable long id){
+
+        return repository.findById(id);
+    }
+
     @GetMapping("/api/kanban")
-    public Iterable<Kanban> kanban() {
+    public Iterable<Kanban> getAllKanbans() {
 
 
         return repository.findAll();
